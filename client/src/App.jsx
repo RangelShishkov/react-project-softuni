12e3ecf99cf8cdd20d8ae8f51838fd61bb62a016
Rangel Shishkov <rshishkov01@gmail.com>
2023-12-01 23:33:13 +0200

    Added footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,20 @@ import { Navbar } from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
 import { Routes, Route } from 'react-router-dom'
+import Footer from './components/footer/Footer'
 
 function App() {
 
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-      </Routes>
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </main>
+      <Footer />
     </>
   )
 }
---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,8 @@
+import styles from './Footer.module.css'
+export default function Footer() {
+  return (
+    <footer className={styles['footer']}>
+      <p>rshishkov©</p>
+    </footer>
+  );
+}
