b1e938f205751d51e18e3fe5767029b0904d3469
Rangel Shishkov <rshishkov01@gmail.com>
2023-12-02 12:36:52 +0200

    Added navbar button css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Navbar } from './components/Navbar'
+import { Navbar } from './components/navbar/Navbar'
 import Home from './components/Home'
 import About from './components/About'
 import { Routes, Route } from 'react-router-dom'
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,23 +1,24 @@
 import { AppBar, Toolbar, IconButton, Typography, Stack, Button } from '@mui/material'
 import FireplaceIcon from '@mui/icons-material/Fireplace';
 import { Link } from 'react-router-dom';
+import styles from './Navbar.module.css';
 
 export const Navbar = () => {
 
     return (
         <AppBar position='static'>
             <Toolbar>
-                <IconButton href='/' size='large' edge="start" color='inherit' aria-label='logo'>
+                <Link to='/'><IconButton size='large' edge="start" color='inherit' aria-label='logo'>
                     <FireplaceIcon />
-                </IconButton>
+                </IconButton></Link>
                 <Typography variant='h5' component='div' sx={{ flexGrow: 1 }}>
-                    Campfire
+                    <Link to='/'><Button color='inherit'>Campfire</Button></Link>
                 </Typography>
                 <Stack direction='row' spacing={2}>
                     <Button color='inherit'>Create post</Button>
                     <Button color='inherit'>Logout</Button>
                     <Button color='inherit'>All posts</Button>
-                    <Button href='/about' color='inherit' >About</Button>
+                    <Link to='/about' className={styles['custom-link']}><Button color='inherit' >About</Button></Link>
                     <Button href='/login' color='inherit' >Login</Button>
                 </Stack>
             </Toolbar>
