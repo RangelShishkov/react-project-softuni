d37e0e7420fe6ac4f8fe2e0a6188427369ae09db
Rangel Shishkov <rshishkov01@gmail.com>
2023-12-07 11:54:04 +0200

    Update Home.jsx

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,29 +1,43 @@
-import { useEffect, useState } from 'react';
-
+import React, { useEffect, useState } from 'react';
 import * as postService from '../../services/postService';
 import Post from "../post/Post";
 
-
-
 function Home() {
     const [posts, setPosts] = useState([]);
-
+    const [error, setError] = useState(null);
+    
     useEffect(() => {
         postService.getAll()
-            .then(result => setPosts(result));
+            .then(result => {
+                // Check if the result is an array before setting the state
+                if (Array.isArray(result)) {
+                    setPosts(result);
+                } else {
+                    setError("Failed to fetch posts. Please try again later.");
+                }
+            })
+            .catch(err => {
+                console.error("Error fetching posts:", err);
+                setError("Failed to fetch posts. Please try again later.");
+            });
     }, []);
+
+    // Validate each post to ensure it has a title and description
     const filteredPosts = posts.filter(post => post.title && post.description);
+
     return (
         <>
-            {filteredPosts.length > 0 ? (
+            {error ? (
+                <h3>{error}</h3>
+            ) : filteredPosts.length > 0 ? (
                 filteredPosts.map(post => (
                     <Post key={post._id} {...post} />
                 ))
             ) : (
-                <h3>No posts yet</h3>
+                <h3>Be the first to create a post!</h3>
             )}
         </>
     );
 }
 
-export default Home
+export default Home;
